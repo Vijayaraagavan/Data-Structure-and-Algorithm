339804e25272c0f4d5fe822ec628921202a163b4
vijay <svijayaraagavan@gmail.com>
2022-11-04 23:48:37 +0530

    Added pop() and sort() for max_heap

--- heap_op.py
+++ heap_op.py
@@ -1,5 +1,6 @@
 from heap import get_heap
-arr = [30, 15, 6, 8, 94]
+# arr = [30, 15, 6, 8, 94]
+arr = [5, 46, 92, 48, 10, 3, 11, 1052, 14, 88, 21]
 # res = get_heap(arr)
 # print(res)
 
@@ -9,6 +10,7 @@ class heap:
         self.data = get_heap(arr)
         self.total_parent = int((len(arr)//2)-1)
         self.total_nodes = len(arr)
+        self.limit = len(arr)-1
 
     def print_heap(self):
         print("Heapified Data Structure => ", self.data)
@@ -19,6 +21,36 @@ class heap:
         index = len(arr)-1
         heap_now(arr, index)
         self.data = arr
+        self.limit = self.limit+1
+
+    def sort(self):
+        arr = self.data
+        for i in range(len(arr)):
+            self.pop_max()
+
+    def pop_max(self):
+        arr = self.data
+        print(arr[0], end=" ")
+        l = self.limit
+        last_node = l
+        swap(arr, 0, l)
+        pop_heap(arr, 0, l)
+        self.limit = l-1
+        self.data = arr
+
+
+def pop_heap(arr, k, l):
+    max = k
+    lef = left(max)
+    rig = right(max)
+    # print(arr, k, lef, rig)
+    if lef < l and arr[k] < arr[lef]:
+        max = lef
+    if rig < l and arr[max] < arr[rig]:
+        max = rig
+    if max != k:
+        swap(arr, max, k)
+        pop_heap(arr, max, l)
 
 
 def left(i):
@@ -34,6 +66,7 @@ def parent(i):
 
 
 def swap(arr, a, b):
+    # print("swapping: ", a, " and ", b)
     tmp = arr[a]
     arr[a] = arr[b]
     arr[b] = tmp
@@ -60,4 +93,9 @@ def heap_now(arr, k):
 obj = heap(arr)
 obj.print_heap()
 obj.insert(95)
+obj.print_heap()
+obj.sort()
+# obj.pop_max()
+# obj.print_heap()
+# obj.pop_max()
 obj.print_heap()
